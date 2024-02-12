25cbe8c222a4454f14eac4895837c645bca253f2
kamshushi <ahmed.elkamshoushy7@yahoo.com>
2024-02-12 20:45:57 +0200

    added landing image styling

---
+++ src/components/Home/LandingSection/ImageSection/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import myPic from "../../../../../public/my-picture2.jpg";
+import classes from "./styles.module.css";
+
+export default function ImageSection() {
+  return (
+    <div className={classes.frameContainer}>
+      <Image src={myPic} alt="profile-picture" className={classes.mainImage} />
+      {/* -------------------------------------------------- */}
+      {/* <figure className={classes.snip1113}>
+        <Image
+          src={myPic}
+          alt="profile-picture"
+          //   className={classes.mainImage}
+        />
+
+        <figcaption>
+          <h3>
+            Ahmed <span>Elkamshoushy</span>
+          </h3>
+          <h4>Dev</h4>
+        </figcaption>
+      </figure> */}
+    </div>
+  );
+}
